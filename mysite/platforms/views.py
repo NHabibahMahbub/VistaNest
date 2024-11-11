7ec47b45cf05c7202b788168dce34233e1e655ca
AfsanaSiddika <afsanaesha584@gmail.com>
2024-11-11 22:30:23 +0600

    added compare_properties

--- mysite/platforms/views.py
+++ mysite/platforms/views.py
@@ -2,6 +2,12 @@ from .models import Platform
 from django.shortcuts import render, redirect
 from . import forms
 
+# platforms/views.py
+from django.shortcuts import render, get_list_or_404
+from .forms import ComparisonForm
+from .models import Platform
+from django.contrib.auth.decorators import login_required
+
 
 def add_platform(request):
     if request.method == "POST":
@@ -34,3 +40,18 @@ def delete_platform(request, p_id):
     Platform.objects.get(pk=p_id).delete()
     return redirect("home")
 
+@login_required(login_url='login')
+def compare_properties(request):
+    if request.method == "POST":
+        form = ComparisonForm(request.POST)
+        if form.is_valid():
+            selected_properties = form.cleaned_data['properties']
+            return render(request, 'compare.html', {
+                'properties': selected_properties
+            })
+    else:
+        form = ComparisonForm()
+
+    return render(request, 'select_properties.html', {
+        'form': form
+    })
