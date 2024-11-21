580272fce36a20b1d5689b91834b82f69f4c146d
AfsanaSiddika <afsanaesha584@gmail.com>
2024-11-21 13:01:03 +0600

    added Compare Properties

--- mysite/platforms/urls.py
+++ mysite/platforms/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.add_platform, name='add-platform'),
     path('update-platform/<int:p_id>', views.update_platform, name='update-platform'),
     path('delete-platform/<int:p_id>', views.delete_platform, name='delete-platform'),
+    path('compare/', views.compare_properties, name='compare_properties'),
 ]
